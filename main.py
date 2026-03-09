7df9658e7085800655c91a53b1a3802261c0870e
thebuzzybee <schoenfeld.robert@web.de>
2026-03-09 14:18:45 +0100

    first sprites

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 ﻿import pygame
+from pygame.examples.cursors import image
+
 from sprites import *
 from config import *
 pygame.init()
 
+
+class Spritesheet:
+    def __init__(self, filename):
+        self.sheet = pygame.image.load(filename).convert()
+        
+    def get_sprite(self, x, y, width, height):
+        sprite = pygame.Surface((width, height))
+        sprite.blit(self.sheet, (0,0),(x, y, width, height))
+        sprite.set_colorkey((0,0,0))
+        return sprite
+      
+        
 class Game:
     def __init__(self):
         pygame.init()
@@ -10,14 +24,32 @@ class Game:
         pygame.display.set_caption("LaBomba")
         self.clock = pygame.time.Clock()
         self.running = True
+        
+        self.player1_spritesheet = Spritesheet("player1_spritesheet.png")
+        
     
+    def createmap(self):
+        for row in range(len(game_map1)):
+            for column in range(len(game_map1[row])):
+                map_code = game_map1[row][column]
+                x = column * tilesize
+                y = row * tilesize
+                if map_code == empty:
+                    continue
+                elif map_code == destructible:
+                    Destructible(self,x,y)
+                elif map_code == indestructible:
+                    Indestructible(self,x,y)
+                elif map_code == player1:
+                    Player1(self,x,y)
+                    
     def new(self):
         self.playing = True
         self.all_sprites = pygame.sprite.LayeredUpdates()
         self.destructibles = pygame.sprite.LayeredUpdates()
         self.indestructibles = pygame.sprite.LayeredUpdates()
+        self.createmap()
         self.playing = True
-        self.player1 = Player1(self, 1, 2)
     
     def updates(self):
         self.all_sprites.update()
--- sprites.py
+++ sprites.py
@@ -6,14 +6,14 @@ class Player1(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.game = game
         self._layer = player1_layer
-        self.groups =self.game.all_sprites
+        self.groups = self.game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
-        self.image = pygame.image.load("Player1_sprite.png")
+        self.image = self.game.player1_spritesheet.get_sprite(0, 0, tilesize, tilesize)
         player1_size = int(tilesize * 2/3)
         self.image = pygame.transform.scale(self.image, (player1_size, player1_size))
         self.rect = self.image.get_rect()
-        self.rect.x = player1_x * tilesize
-        self.rect.y = player1_y * tilesize
+        self.rect.x = player1_x
+        self.rect.y = player1_y
         self.player1_facing = "down"
         
     def update(self):
@@ -35,3 +35,38 @@ class Player1(pygame.sprite.Sprite):
             self.rect.y += player1_velocity
             self.player1_facing = "down"
             
+class Destructible(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = destructible_layer
+        self.groups = self.game.all_sprites, self.game.destructibles
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        
+        self.x = x
+        self.y = y
+        self.width = tilesize
+        self.height = tilesize
+        
+        self.image = pygame.image.load("brown_square.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+class Indestructible(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = indestructible_layer
+        self.groups =self.game.all_sprites, self.game.indestructibles
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x
+        self.y = y
+        self.width = tilesize
+        self.height = tilesize
+
+        self.image = pygame.image.load("black_square.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
